e3f548d16ad0bc0058465638cae5964e4e29a49d
Paulius88 <82173700+Paulius88@users.noreply.github.com>
2021-12-29 20:53:58 +0200

    Add files via upload

---
+++ classwork/ArrayHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+class ArrayHelper
+{
+	public static function getArraySum($array)
+{
+
+	return array_sum($array;
+
+	}
+
+public static function getArrayAvarage($array)
+{
+	$sum = self::getArrayAvarage($array);
+	return $sum / count($array);
+	}
+}
+
